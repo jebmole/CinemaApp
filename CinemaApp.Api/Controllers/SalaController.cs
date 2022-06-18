495cba047e5d125e5fe4819d19b1b9bac46a6628
Jonathan Estrada <jonathan.estrada@unosquare.com>
2022-06-17 19:31:15 -0500

    Se agrega documentación con Swagger

--- CinemaApp.Api/Controllers/SalaController.cs
+++ CinemaApp.Api/Controllers/SalaController.cs
@@ -1,13 +1,18 @@
 ﻿using CinemaApp.Application.Interfaces;
 using CinemaApp.Application.Requests;
+using CinemaApp.Application.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 
 namespace CinemaApp.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
+    [Produces("application/json")]
     public class SalaController : ControllerBase
     {
         private readonly ISalaService _service;
@@ -17,19 +22,42 @@ namespace CinemaApp.Api.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Retorna un listado con todas las salas registradas
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<SalaResponse>))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ProblemDetails))]
         public IActionResult Get()
         {
             return Ok(_service.GetSalas());
         }
 
+        /// <summary>
+        /// Permite consultar la información de una sala por su id
+        /// </summary>
+        /// <param name="request">Identificador de la sala a buscar</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(SalaResponse))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Get([FromRoute] GetSalaByIdRequest request)
         {
             return Ok(_service.GetSalaById(request.Id));
         }
 
+        /// <summary>
+        /// Permite insertar una sala
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Post(CreateSalaRequest request)
         {
             _service.InsertSala(request);
@@ -37,6 +65,9 @@ namespace CinemaApp.Api.Controllers
         }
 
         [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Put(UpdateSalaRequest request)
         {
             _service.UpdateSala(request);
@@ -44,6 +75,9 @@ namespace CinemaApp.Api.Controllers
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Delete([FromRoute] DeleteSalaRequest request)
         {
             _service.DeleteSala(request.Id);
--- CinemaApp.Api/Startup.cs
+++ CinemaApp.Api/Startup.cs
@@ -20,7 +20,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -81,6 +83,23 @@ namespace CinemaApp.Api
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("asdfawdfsdfqwerwefcwaefewtwassdas"))
                 };
             });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Cinema API",
+                    Description = "Estas son los endpoints disponibles para la API Cinema"
+                });
+
+                //Obtener de forma dinamica el nombre del archivo
+                var nombreArchivo = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+                //Creamos una variable con la ruta completa del archivo
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, nombreArchivo);
+
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -98,6 +117,16 @@ namespace CinemaApp.Api
             //Se registra la autenticación
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                //Especificar la url en donde se encuentra el swagger de la API
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cinema API v1");
+                
+                //Especificar que swagger sea la pagina por defecto
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
